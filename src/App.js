5cf7154232b069011cc8a253cd036e0d27745033
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-16 23:13:36 +0200

    Created and Instantiated theme and TopBar with colorMode feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-	return <div className='App'>App</div>
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { ColorModeContext, useMode } from './theme'
+import TopBar from './scenes/global/TopBar'
+
+const App = () => {
+	const [theme, colorMode] = useMode()
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<main className='content'>
+						<TopBar />
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	)
 }
 
 export default App
